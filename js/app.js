6f56a55c89ae0ca59c32cb86fcf68bd1c07b60b6
Justin Benjamin <justinjbenjamin@gmail.com>
2023-06-10 16:01:23 -0700

    Driver Session 1

--- js/app.js
+++ js/app.js
@@ -20,13 +20,21 @@ AppState.prototype.instantiateProducts = function () {
 
 AppState.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save product data to local storage
-}
+    let stringifiedProducts = JSON.stringify(this.allProducts)
+    localStorage.setItem('allProducts', stringifiedProducts);
+      } 
+
 
 AppState.prototype.loadItems = function () {
 
   // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
 
-  this.instantiateProducts();
+// Check to see if there are products in local storage
+// If there are products in local storage, then reintantiate your product arrayw with those products
+// If there are not products inside, then use instantiate products methods to fill local storage with new products
+
+    // if localStorage.getItem 
+
 
 }
 
@@ -37,3 +45,4 @@ function Product(name, fileExtension = 'jpg') {
   this.timesClicked = 0;
   this.timesShown = 0;
 }
+
--- js/chart.js
+++ js/chart.js
@@ -10,7 +10,67 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
+
+
+const AppState = new AppState();
+
+function AppState(){
+ 
+AppState.prototype.instantiateProducts();    
+AppState.loaditems();
+AppState.allproducts = [];
+
+
+let views = [] 
+let labels = []
+let votes = []
+
+let productNamesString = JSON.stringify(productNames);
+localStorage.setItem('productNames', productNamesString);
+
+for(let i = 0; i < productNames.length; i++){
+  productNames.push(allProducts[i].name);
+  proudctVotes.push(allProuducts[i].votes);
+  productViews.push(allProducts[i].views);
+}
+
+const ctx = document.getElementById('chart');
+
 function renderChart() {
+
+Chart.defaults.color = 'black';
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: productNames,
+    datasets: [{
+      label: '# of Votes',
+      data: productVotes,
+      borderWidth: 2,
+      backgroundColor: '#e76f51',
+      borderColor: 'black',
+    },
+    {
+      label: '# of Views',
+      data: productViews,
+      borderWidth: 2,
+      backgroundColor: '#e9c46a',
+      borderColor: 'black',
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+
+
+}
 }
 
 renderChart();
